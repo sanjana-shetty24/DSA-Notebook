8dd25f20c05eb722e5d4952c845e51f760d60cb1
norvexis <sanjanashetty2411@gmail.com>
2026-07-24 20:42:24 +0530

    Time: 1 ms (98.07%), Space: 47.5 MB (80.96%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,49 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean firstColZero = false;
+
+        // Step 1: Mark rows and columns
+        for (int i = 0; i < m; i++) {
+
+            if (matrix[i][0] == 0)
+                firstColZero = true;
+
+            for (int j = 1; j < n; j++) {
+
+                if (matrix[i][j] == 0) {
+
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Step 2: Fill cells using markers
+        for (int i = 1; i < m; i++) {
+
+            for (int j = 1; j < n; j++) {
+
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+        }
+
+        // Step 3: First row
+        if (matrix[0][0] == 0) {
+
+            for (int j = 0; j < n; j++)
+                matrix[0][j] = 0;
+        }
+
+        // Step 4: First column
+        if (firstColZero) {
+
+            for (int i = 0; i < m; i++)
+                matrix[i][0] = 0;
+        }
+    }
+}
