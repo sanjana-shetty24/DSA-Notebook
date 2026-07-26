e72950c3a9f759fbc1548ada16d0740b92111d94
norvexis <sanjanashetty2411@gmail.com>
2026-07-26 17:13:28 +0530

    Time: 32 ms (45.77%), Space: 155.7 MB (25.68%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+
+        int n = nums.length;
+        int[] ans = new int[n - k + 1];
+
+        Deque<Integer> dq = new LinkedList<>();
+        int idx = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            // Remove indices outside the window
+            while (!dq.isEmpty() && dq.peekFirst() <= i - k) {
+                dq.pollFirst();
+            }
+
+            // Remove smaller elements
+            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
+                dq.pollLast();
+            }
+
+            // Add current index
+            dq.offerLast(i);
+
+            // Store maximum
+            if (i >= k - 1) {
+                ans[idx++] = nums[dq.peekFirst()];
+            }
+        }
+
+        return ans;
+    }
+}
